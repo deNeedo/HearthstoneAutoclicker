1e1f030512fb62a6568e819df24d5b00157cea9f
deNeedo <jkudrys@protonmail.com>
2023-05-21 23:36:43 +0200

    Added option to select mercenaries

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import pynput, time
-fights = 3; turns = 3; m = 0; n = 0; b = 0
+import pynput, time, mercenaries
+
 def SelectBounty():
     pynput.mouse.Controller().position = (1504, 855); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
-def SelectParty():
+
+def SelectParty(counter):
     pynput.mouse.Controller().position = (1415, 912); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+    if (counter == 0):
+        pynput.mouse.Controller().position = (845, 631); time.sleep(0.5)
+        pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def SelectFight():
     pynput.mouse.Controller().position = (467, 505); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
@@ -19,11 +24,13 @@ def SelectFight():
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
     pynput.mouse.Controller().position = (1530, 845); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def SelectBossFight():
     pynput.mouse.Controller().position = (768, 219); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
     pynput.mouse.Controller().position = (1530, 845); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def CollectTreasure():
     pynput.mouse.Controller().position = (723, 741); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
@@ -33,9 +40,11 @@ def CollectTreasure():
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(2)
     pynput.mouse.Controller().position = (996, 630); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def Finish():
     pynput.mouse.Controller().position = (938, 880); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def Attack():
     pynput.mouse.Controller().position = (965, 722); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
@@ -43,65 +52,91 @@ def Attack():
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
     pynput.mouse.Controller().position = (995, 291); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def ButtonClick():
     pynput.mouse.Controller().position = (1567, 503); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
 def FinishFight():
     pynput.mouse.Controller().position = (100, 100); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(7)
+
 def SelectPower():
     pynput.mouse.Controller().position = (791, 521); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
     pynput.mouse.Controller().position = (1127, 925); time.sleep(0.5)
     pynput.mouse.Controller().click(pynput.mouse.Button.left, 1); time.sleep(0.5)
+
+def Await():
+    t = 60*28; time.sleep(t)
+
+def Initialize(mercenary):
+    if mercenary == "Xyrella":
+        return mercenaries.Xyrella()
+    elif mercenary == "Millhouse":
+        return mercenaries.Millhouse()
+    else:
+        return None
+    
 def Main():
-    time.sleep(5)
-    while 1:
-        # every bounty #
-        SelectBounty()
-        time.sleep(3)
-        SelectParty()
-        time.sleep(7)
-        m = 0
-        while m < fights:
-            # every normal fight #
-            SelectFight()
+    fights = 3; counter = 0; 
+    mercenary = input("Provide a name of a mercenary you wish to start the run with: ")
+    mercenary = Initialize(mercenary)
+    if (mercenary == None):
+        print("No config for provided mercenary!")
+    else:
+        print("Starting run with: " + mercenary.name)
+        time.sleep(5)
+        while 1:
+            # every bounty #
+            print("Bounties completed in this run: " + str(counter))
+            SelectBounty()
+            time.sleep(3)
+            SelectParty(counter)
+            time.sleep(7)
+            m = 0
+            while m < fights:
+                # every normal fight #
+                SelectFight()
+                time.sleep(20)
+                # Await() # for farming exp instead of coins
+                ButtonClick()
+                time.sleep(10)
+                n = 0
+                while n < mercenary.turns:
+                    # every turn #
+                    Attack()
+                    time.sleep(1)
+                    ButtonClick()
+                    time.sleep(12)
+                    n += 1
+                    # every turn #
+                FinishFight()
+                time.sleep(3)
+                SelectPower()
+                time.sleep(3)
+                m += 1
+                # every normal fight #
+            SelectBossFight()
             time.sleep(20)
+            # Await() # for farming exp instead of coins
             ButtonClick()
             time.sleep(10)
-            n = 0
-            while n < turns:
+            b = 0
+            while b < mercenary.boss_turns:
                 # every turn #
                 Attack()
                 time.sleep(1)
                 ButtonClick()
-                time.sleep(11)
-                n += 1
+                time.sleep(12)
+                b += 1
                 # every turn #
             FinishFight()
-            time.sleep(5)
-            SelectPower()
+            time.sleep(10)
+            CollectTreasure()
             time.sleep(3)
-            m += 1
-            # every normal fight #
-        SelectBossFight()
-        time.sleep(20)
-        ButtonClick()
-        time.sleep(10)
-        b = 0
-        while b < turns:
-            # every turn #
-            Attack()
-            time.sleep(1)
-            ButtonClick()
-            time.sleep(11)
-            b += 1
-            # every turn #
-        FinishFight()
-        time.sleep(15)
-        CollectTreasure()
-        time.sleep(3)
-        Finish()
-        time.sleep(3)
-        # every bounty #
+            Finish()
+            time.sleep(3)
+            counter += 1
+            # every bounty #
 Main()
---
+++ mercenaries.py
@@ -0,0 +1,11 @@
+class Xyrella:
+    def __init__(self):
+        self.name = "Xyrella"
+        self.turns = 3
+        self.boss_turns = 4
+
+class Millhouse:
+    def __init__(self):
+        self.name = "Millhouse"
+        self.turns = 1
+        self.boss_turns = 3
